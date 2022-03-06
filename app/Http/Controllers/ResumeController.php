3628c4e7ccc051af157b98b036bfa8354e6d3bef
Matias Olivieri <matiasolivieri34@gmail.com>
2022-03-06 17:58:00 -0300

    Initial Commit

---
+++ app/Http/Controllers/ResumeController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resume;
+use Illuminate\Http\Request;
+
+class ResumeController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $resumes = auth()->user()->resumes;
+        return view('resumes.index', compact('resumes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data = 'test 12345';
+        return view('resumes.create', ['data' => $data]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = auth()->user();
+        $resume = $user->resumes()->where('title', $request->title) ->first();
+        $resume = $user->resumes()->create([
+            'title' => $request ['title'],
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+
+        return redirect()->route('resumes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Resume $resume)
+    {
+        return view('resumes.edit', compact('resume'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Resume $resume)
+    {
+        //
+    }
+}
